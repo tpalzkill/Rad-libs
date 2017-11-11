c77b680d02fed0e8a0a8ac5f9d91532fa8fa96de
Thomas Palzkill <tpalzkilll@gmail.com>
2017-11-11 09:55:26 -0600

    baby steps

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // j
-let $xhr = $.getJSON('https://api.whatdoestrumpthink.com/api/v1/quotes/random');
 
-$xhr.done(function(data) {
-  if ($xhr.status !== 200) {
+
+let trumpThink = $.getJSON('https://api.whatdoestrumpthink.com/api/v1/quotes/random');
+
+trumpThink.done(function(data) {
+  if (trumpThink.status !== 200) {
     return;
   }
 
@@ -10,33 +12,36 @@ $xhr.done(function(data) {
   console.log(trump[0]);
 });
 
-let $otherShit = $.getJSON('https://icanhazdadjoke.com/');
 
-$otherShit.done(function(data) {
-  if ($otherShit.status !== 200) {
-    return;
-  }
 
-  let dadJoke = Object.values(data);
-  console.log(dadJoke[1]);
-});
 
-let $numeroTres = $.getJSON('https://favqs.com/api/qotd');
-
-$numeroTres.done(function(data) {
-  if ($numeroTres.status !== 200) {
-    return;
-  }
-let inspiration = Object.values(data);
-let actualQuote = inspiration[1];
-console.log(actualQuote.body);
-});
-let $numeroQuat = $.getJSON('http://ron-swanson-quotes.herokuapp.com/v2/quotes');
-
-$numeroQuat.done(function(data) {
-  if ($numeroQuat.status !== 200) {
-    return;
-  }
-
-  console.log(data[0]);
-});
+// let hazDadJoke = $.getJSON('https://icanhazdadjoke.com/');
+//
+// hazDadJoke.done(function(data) {
+//   if (hazDadJoke.status !== 200) {
+//     return;
+//   }
+//
+//   let dadJoke = Object.values(data);
+//   console.log(dadJoke[1]);
+// });
+//
+// let inspirational = $.getJSON('https://favqs.com/api/qotd');
+//
+// inspirational.done(function(data) {
+//   if (inspirational.status !== 200) {
+//     return;
+//   }
+// let inspiration = Object.values(data);
+// let actualQuote = inspiration[1];
+// console.log(actualQuote.body);
+// });
+// let ronSwan = $.getJSON('http://ron-swanson-quotes.herokuapp.com/v2/quotes');
+//
+// ronSwan.done(function(data) {
+//   if (ronSwan.status !== 200) {
+//     return;
+//   }
+//
+//   console.log(data[0]);
+// });
