bb7e7ec424a271b4452f2ddc3fa3e19aa3b8e873
Thomas Palzkill <tpalzkilll@gmail.com>
2017-11-11 15:25:53 -0600

    select works, form disappears on submission

--- index.js
+++ index.js
@@ -1,47 +1,53 @@
-// j
+let selection = [];
+$("#btnGet").click(function(event) {
+  event.preventDefault();
+  let butthead = $('#sourceSelector option:selected').text();
+  selection = [];
+  selection.push(butthead);
+  if (butthead === "Trump Quote") {
+    let trumpThink = $.getJSON('https://api.whatdoestrumpthink.com/api/v1/quotes/random');
 
+    trumpThink.done(function(data) {
+      if (trumpThink.status !== 200) {
+        return;
+      }
 
-let trumpThink = $.getJSON('https://api.whatdoestrumpthink.com/api/v1/quotes/random');
+      let trump = Object.values(data);
+      console.log(trump[0]);
+    });
+  } else if (butthead === "Inspirational Quote") {
+    let inspirational = $.getJSON('https://favqs.com/api/qotd');
 
-trumpThink.done(function(data) {
-  if (trumpThink.status !== 200) {
-    return;
+    inspirational.done(function(data) {
+      if (inspirational.status !== 200) {
+        return;
+      }
+
+      let inspiration = Object.values(data);
+      let actualQuote = inspiration[1];
+      console.log(actualQuote.body);
+    });
+  } else if (butthead === "Dad Joke") {
+    let hazDadJoke = $.getJSON('https://icanhazdadjoke.com/');
+
+    hazDadJoke.done(function(data) {
+      if (hazDadJoke.status !== 200) {
+        return;
+      }
+
+      let dadJoke = Object.values(data);
+      console.log(dadJoke[1]);
+    });
+  } else if (butthead === "Ron Swanson Quote") {
+    let ronSwan = $.getJSON('http://ron-swanson-quotes.herokuapp.com/v2/quotes');
+
+    ronSwan.done(function(data) {
+      if (ronSwan.status !== 200) {
+        return;
+      }
+
+      console.log(data[0]);
+    });
   }
-
-  let trump = Object.values(data);
-  console.log(trump[0]);
+  $("form").fadeOut();
 });
-
-
-
-
-// let hazDadJoke = $.getJSON('https://icanhazdadjoke.com/');
-//
-// hazDadJoke.done(function(data) {
-//   if (hazDadJoke.status !== 200) {
-//     return;
-//   }
-//
-//   let dadJoke = Object.values(data);
-//   console.log(dadJoke[1]);
-// });
-//
-// let inspirational = $.getJSON('https://favqs.com/api/qotd');
-//
-// inspirational.done(function(data) {
-//   if (inspirational.status !== 200) {
-//     return;
-//   }
-// let inspiration = Object.values(data);
-// let actualQuote = inspiration[1];
-// console.log(actualQuote.body);
-// });
-// let ronSwan = $.getJSON('http://ron-swanson-quotes.herokuapp.com/v2/quotes');
-//
-// ronSwan.done(function(data) {
-//   if (ronSwan.status !== 200) {
-//     return;
-//   }
-//
-//   console.log(data[0]);
-// });
